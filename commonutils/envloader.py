49f0f998de5577881a09d18330d5db5171cf8bd0
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2024-06-18 20:14:23 +0530

    Update envloader.py

--- commonutils/envloader.py
+++ commonutils/envloader.py
@@ -4,13 +4,44 @@ import json
 from typing import Optional, Callable
 
 
+__all__ = (
+  "load",
+  "reload",
+  "unload"
+)
+
 def load(
   file: Optional[str] = ".env.json",
   loader_func: Optional[Callable] = json.load
-):
+) -> None:
+  """Load environment variables from a file"""
   with open(file, "r") as f:
     os.environ.update(
       loader_func(f)
     )
 
 reload = load
+
+def unload(
+  file: Optional[str] = ".env.json",
+  loader_func: Optional[Callable] = json.load,
+  keep_changes: Optional[bool] = False
+) -> None:
+  """Unload environment variables from a file"""
+  with open(file, "r") as f:
+    data = loader_func(f)
+
+  MISSING = object()
+  for key, value in data.items():
+    env_val = os.environ.get(key, MISSING)
+    
+    if env_val is MISSING:
+      continue
+      
+    if keep_changes:
+      if env_val != value:
+        continue
+    
+    del os.environ[key]
+
+  
