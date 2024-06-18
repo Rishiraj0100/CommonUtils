2b10fbc7e2396d18aa27da6f6b31b5d883b93e74
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2024-06-18 19:46:23 +0530

    Update __init__.py

--- commonutils/__init__.py
+++ commonutils/__init__.py
@@ -1,5 +1,9 @@
-from .envloader import load as load_env
+from .envloader import (
+  load as load_env,
+  reload as reload_env
+)
 
 
-def _(**kwargs):
+def _(**kwargs) -> None:
+  """Main Function"""
   load_env(**kwargs.get("load_env",{})
